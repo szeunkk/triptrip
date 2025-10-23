f7964ae6fd03da44fb197194e9bcf4e9f911ed1a
szeunkk <szeunkk@gmail.com>
2025-10-23 09:42:38 +0900

    feat(boards-detail): 게시글 상세 페이지 댓글 영역 추가

--- src/app/boards/[boardId]/page.tsx
+++ src/app/boards/[boardId]/page.tsx
@@ -1,5 +1,12 @@
+import BoardsComment from "@/components/boards-comment";
 import { BoardsDetail } from "@/components/boards-detail";
+import styles from "./styles.module.css";
 
 export default function BoardsDetailPage() {
-  return <BoardsDetail />;
+  return (
+    <div className={styles.BoardsDetail}>
+      <BoardsDetail />
+      <BoardsComment />
+    </div>
+  );
 }
